e260014ee8cd6c4112539b972609c0d87500afa5
Sophifc <>
2022-03-17 23:02:22 +0000

    new try

--- list.c
+++ list.c
@@ -90,7 +90,7 @@ void pushBack(List * list, void * data) {
 }
 
 void pushCurrent(List * list, void * data) {
-  /*Node* nodo=createNode((void*)data);
+  Node* nodo=createNode((void*)data);
   if(list->head==NULL){
     list->head=nodo;
   }
@@ -98,7 +98,7 @@ void pushCurrent(List * list, void * data) {
     list->head->prev=nodo;
     nodo->next=list->head;
     list->head=nodo;
-  }*/
+  }
 }
 
 void * popFront(List * list) {
